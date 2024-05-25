bade0294337627c85cdb1063b0b2bbd2d3d5faba
deepanshoo <sharmadeepanshu918@gmail.com>
2024-05-25 22:33:28 +0530

    Third Commit

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(cors(
       credentials: true
   }
 ));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 
 // Connect to MongoDB
